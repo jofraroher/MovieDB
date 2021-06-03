45d775c995b26a6f570962734d4373e60e2a5add
Jose Francisco Rosales Hernandez <jose.rosales@hugoapp.com>
2021-06-02 23:10:35 -0500

    Create Xcode project

---
+++ MovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDB
+//
+//  Created by Jose Francisco Rosales Hernandez on 02/06/21.
+//  Copyright © 2021 Jose Francisco Rosales Hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
