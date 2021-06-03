fa44f7cfefe7b3ea4094dc7c8130c973a20fbbee
Jose Francisco Rosales Hernandez <jose.rosales@hugoapp.com>
2021-06-03 00:33:53 -0500

    The common class is added to be able to change the color of the tab bar.

---
+++ MovieDB/Utils/HexToColor.swift
@@ -0,0 +1,25 @@
+//
+//  HexToColor.swift
+//  MovieDB
+//
+//  Created by Jose Francisco Rosales Hernandez on 02/06/21.
+//  Copyright © 2021 Jose Francisco Rosales Hernandez. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    convenience init?(hexString: String) {
+        var chars = Array(hexString.hasPrefix("#") ? hexString.dropFirst() : hexString[...])
+        switch chars.count {
+        case 3: chars = chars.flatMap { [$0, $0] }; fallthrough
+        case 6: chars = ["F","F"] + chars
+        case 8: break
+        default: return nil
+        }
+        self.init(red: .init(strtoul(String(chars[2...3]), nil, 16)) / 255,
+                green: .init(strtoul(String(chars[4...5]), nil, 16)) / 255,
+                 blue: .init(strtoul(String(chars[6...7]), nil, 16)) / 255,
+                alpha: .init(strtoul(String(chars[0...1]), nil, 16)) / 255)
+    }
+}
